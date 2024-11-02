675546480072215d58a95b75b87f23d39a2994df
AlazarBekele <alazarthe70@gmail.com>
2024-11-02 16:46:08 +0300

    Finsh Model class

--- Base/admin.py
+++ Base/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import School_Deployment
+from .models import (
+  School_Deployment,
+  Catagory,
+)
 # Register your models here.
 
 admin.site.register(School_Deployment)
+admin.site.register(Catagory)
--- Base/models.py
+++ Base/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Catagory (models.Model):
+
+  name = models.CharField (max_length=70)
+
+  def __str__(self) -> str:
+    return self.name
+
 class School_Deployment(models.Model):
 
   first_name = models.CharField (max_length=30)
   last_name = models.CharField (max_length=40)
   image = models.ImageField(upload_to='photo/')
   description = models.TextField()
+  catagory = models.ForeignKey(Catagory, on_delete=models.SET_NULL, null=True)
 
   def __str__(self) -> str:
     return self.first_name + " " + self.last_name
