155672c079e7be3ac1196b3d4b5995344add22a1
AlazarBekele <alazarthe70@gmail.com>
2024-11-02 16:38:03 +0300

    Succssusfully Deployed

--- Base/admin.py
+++ Base/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import School_Deployment
 # Register your models here.
+
+admin.site.register(School_Deployment)
--- Base/models.py
+++ Base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class School_Deployment(models.Model):
+
+  first_name = models.CharField (max_length=30)
+  last_name = models.CharField (max_length=40)
+  image = models.ImageField(upload_to='photo/')
+  description = models.TextField()
+
+  def __str__(self) -> str:
+    return self.first_name + " " + self.last_name
